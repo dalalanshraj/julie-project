f243aefd98831485099e597325070f014aa5fbcc
dalalanshraj <dalalanshraj@gmail.com>
2026-05-23 00:36:46 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -43,11 +43,10 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(null, false);
       }
     },
 
